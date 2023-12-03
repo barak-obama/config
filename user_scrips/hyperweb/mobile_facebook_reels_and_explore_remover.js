4b4e050e1fa5fb2fd7b0838e8bef3985a9fb231e
barak-obama <barak.oh16@gmail.com>
2023-12-03 23:16:23 +0200

    Update mobile_facebook_reels_and_explore_remover.js

--- user_scrips/hyperweb/mobile_facebook_reels_and_explore_remover.js
+++ user_scrips/hyperweb/mobile_facebook_reels_and_explore_remover.js
@@ -2,75 +2,22 @@ if (window.location.hostname === "m.facebook.com") {
     console.log("in reels")
     if (window.location.pathname.includes("/reel")) {
                 window.location.pathname = "/";
-        //         // const reelsFeed = document.querySelector(reelsFeedSelector);
-    
-        //         // if (reelsFeed) {
-        //         //     reelsFeed.remove();
-        //         // }
-            }
-    // const reelsButtonSelector = "div[style='transform: translateX(0px);'] > div[style='transform: translateX(0px);'] > div > div:has(a[href='/reels/'])";
-    // const reelsFeedSelector = 'section > main div';
-    // // const loadingWheelSelector = 'div[data-visualcompletion="loading-state"]'
+    }
 
-    // const reelsButton = document.querySelector(reelsButtonSelector);
+    const reels_observer = new MutationObserver((mutations, obs) => {
+        const reelsButton = document.querySelector(reelsButtonSelector);
 
-    //     if(reelsButton){
-    //         reelsButton.remove()
-    //     }
-
-    // const reels_observer = new MutationObserver((mutations, obs) => {
-    //     const reelsButton = document.querySelector(reelsButtonSelector);
-
-    //     if(reelsButton){
-    //         reelsButton.remove()
-    //     }
-
-    //     if (window.location.pathname.contains("/reels/")) {
-    //         window.location.pathname = "/";
-    //         // const reelsFeed = document.querySelector(reelsFeedSelector);
-
-    //         // if (reelsFeed) {
-    //         //     reelsFeed.remove();
-    //         // }
-    //     }
+        if (window.location.pathname.includes("/reel")) {
+                window.location.pathname = "/";
+        }
 
         
-    // });
+    });
 
-    // reels_observer.observe(document, {
-    //     childList: true,
-    //     subtree: true
-    // });
-
-
-
-    // const exploreButtonSelector = 'a[href="/explore/"]';
-    // const exploreFeedSelector = 'section > main div[style*="flex-direction: column"]';
-    // const loadingWheelSelector = 'div[data-visualcompletion="loading-state"]'
-
-    // const explore_observer = new MutationObserver((mutations, obs) => {
-    //     const exploreButton = document.querySelector(exploreButtonSelector);
-    //     if(exploreButton){
-    //         //exploreButton.remove()
-    //     }
-
-    //     if (window.location.pathname == "/explore/") {
-    //         const exploreFeed = document.querySelector(exploreFeedSelector);
-
-    //         if (exploreFeed) {
-    //             exploreFeed.remove();
-    //         }
-    //         const loadingWheelButton = document.querySelector(loadingWheelSelector);
-    //         if (loadingWheelButton) {
-    //             loadingWheelButton.remove();
-    //         }
-    //     }
-    // });
-
-    // explore_observer.observe(document, {
-    //     childList: true,
-    //     subtree: true
-    // });
+    reels_observer.observe(document, {
+        childList: true,
+        subtree: true
+    });
 
 
 
