5c3cf6d0fde8a3f1ad74850fb68781dc5b24f0ac
barak-obama <barak.oh16@gmail.com>
2023-12-03 23:16:50 +0200

    Update mobile_facebook_reels_and_explore_remover.js

--- user_scrips/hyperweb/mobile_facebook_reels_and_explore_remover.js
+++ user_scrips/hyperweb/mobile_facebook_reels_and_explore_remover.js
@@ -1,13 +1,11 @@
 if (window.location.hostname === "m.facebook.com") {
-    console.log("in reels")
-    if (window.location.pathname.includes("/reel")) {
-                window.location.pathname = "/";
-    }
 
     const reels_observer = new MutationObserver((mutations, obs) => {
         const reelsButton = document.querySelector(reelsButtonSelector);
 
+        
         if (window.location.pathname.includes("/reel")) {
+                console.log("in reels")
                 window.location.pathname = "/";
         }
 
